9b66a3b0db7ba0ed7245a956a8f24839a00e81f8
Your Name <assahuaayush3@gmail.com>
2024-07-26 12:40:15 +0530

    heros js

--- src/utils/dark-mode.js
+++ src/utils/dark-mode.js
@@ -5,16 +5,24 @@ const darkMode = () => {
     const theme = localStorage.getItem('theme');
 
     // On mount
-    theme && document.body.classList.add(theme);
+    if (theme) {
+        document.body.classList.add(theme);
+        if (theme === 'light-mode') {
+            document.querySelector('.hero').style.backgroundImage = "url('hero-light.jpg')";
+        }
+    }
 
     // Handlers
     const handleThemeToggle = () => {
         document.body.classList.toggle('light-mode');
+        const heroSection = document.querySelector('.hero');
         if (document.body.classList.contains('light-mode')) {
             localStorage.setItem('theme', 'light-mode');
+            heroSection.style.backgroundImage = "url('/hero-light.jpg')";
         } else {
             localStorage.removeItem('theme');
             document.body.removeAttribute('class');
+            heroSection.style.backgroundImage = "url('/heros.jpg')";
         }
     };
 
